cdf1875a7c60e6259273384518d671cd4c674899
DracotMolver <dracotm25@gmail.com>
2020-09-06 19:09:58 -0300

    added sort

---
+++ GoLangCourse/11_class/sort/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s: %d", p.Name, p.Age)
+}
+
+// ByAge implements sort.Interface for []Person based on
+// the Age field.
+type ByAge []Person
+
+func (a ByAge) Len() int {
+	return len(a)
+}
+
+func (a ByAge) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByAge) Less(i, j int) bool {
+	return a[i].Age < a[j].Age
+}
+
+func main() {
+
+	s := []int{2, 5, 6, 3, 1, 4}
+
+	sort.Ints(s)
+
+	fmt.Println(s)
+
+	people := []Person{
+		{"Bob", 31},
+		{"John", 42},
+		{"Michael", 17},
+		{"Jenny", 26},
+	}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+}
